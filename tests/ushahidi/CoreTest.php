ff3d484da35f969890d98dcab016f8718584adcb
Emmanuel Kala <emkala@gmail.com>
2012-10-06 08:35:17 +0300

    Unit tests for Ushahidi_Core
    * Better code coverage for the Ushahidi_Core library. Both valid and
    invalid inputs are now incorporated in the test data

--- tests/ushahidi/CoreTest.php
+++ tests/ushahidi/CoreTest.php
@@ -19,7 +19,8 @@ class Ushahidi_CoreTest extends Unittest_TestCase {
 	{
 		return array(
 			array(
-				'http://stable.ushahidi.com'
+				'http://stable.ushahidi.com',
+				'http://www.google.com'
 			)
 		);
 	}
@@ -31,10 +32,45 @@ class Ushahidi_CoreTest extends Unittest_TestCase {
 	 * @covers Ushahidi_Core::is_compatible_deployment
 	 * @dataProvider provider_is_compatible_deployment
 	 */
-	public function test_is_compatible_deployment($url)
+	public function test_is_compatible_deployment($url, $bad_url)
 	{
 		$is_compatible = Ushahidi_Core::is_compatible_deployment($url);
 		
 		$this->assertTrue($is_compatible);
+		
+		$not_compatible = Ushahidi_Core::is_compatible_deployment($bad_url);
+		$this->assertFalse($not_compatible);
+	}
+
+	/**
+	 * Provides data for test_get_request_url
+	 *
+	 * @return array
+	 */
+	public function provider_get_request_url()
+	{
+		return array(
+			array(
+				'http://stable.ushahidi.com/',
+				'api?task=categories',
+				'http://stable.ushahidi.com/api?task=categories'
+			)
+		);
+	}
+
+	/**
+	 * Tests concatenation of the deployment url and a segment to
+	 * produce a full request uri
+	 *
+	 * @covers Ushahidi_Core::get_request_url
+	 * @dataProvider provider_get_request_url
+	 */
+	public function test_get_request_url($url, $endpoint, $expected)
+	{
+		// Build out the URL
+		$request_url = Ushahidi_Core::get_request_url($url, $endpoint);
+
+		// Return url string should match the on in $expected
+		$this->assertEquals($request_url, $expected);
 	}
 }
